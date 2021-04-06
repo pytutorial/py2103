0f1d61c75ee0cfd22521c5e2524430e4a0823f7b
Trung Doan <doanptand@gmail.com>
2021-04-06 21:24:11 +0700

    Update

---
+++ Day14/connect.py
@@ -0,0 +1,11 @@
+import MySQLdb
+
+db_host = '34.122.175.106'
+db_user = 'admin'
+db_pass = 'abc@123'
+db_name = 'py2103'
+
+conn = MySQLdb.connect(
+    db_host, db_user, db_pass, db_name
+)
+print('Connected')
