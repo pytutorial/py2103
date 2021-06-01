286a816d249ab194921fcff295ecde86ac9c6598
Tung Duong <duongthanhtungvn01@gmail.com>
2021-06-01 19:56:54 +0700

    Update vd2

--- Day24/vd2/app.py
+++ Day24/vd2/app.py
@@ -2,9 +2,13 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/')  # 127.0.0.1:5000?name=Nguyen+Van+A
+@app.route('/')  # 127.0.0.1:5000
 def index():
+    return render_template('index.html') 
+
+@app.route('/hello')
+def hello():
     name = request.args.get('name', '') 
-    return render_template('index.html', username=name) 
+    return render_template('hello.html', name=name)
 
 app.run(debug=True)
