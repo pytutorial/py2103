b4f8c0110f07ee08c22f55cc5196c7d1036d222e
Tung Duong <duongthanhtungvn01@gmail.com>
2021-06-01 21:48:06 +0700

    Update

--- Day24/vd4/app.py
+++ Day24/vd4/app.py
@@ -1,7 +1,6 @@
 #pip install pymongo
-import re
 from pymongo import MongoClient
-
+from bson.objectid import ObjectId
 db_host = '34.70.69.231'
 db_user = 'mongo'
 db_pass = 'abc@123'
@@ -17,28 +16,40 @@ def index():
     productList = db.product.find()
     return render_template('index.html', productList=productList)
 
-@app.route('/add-product')
-def addProduct():
-    return render_template('product_form.html')
-
 @app.route('/save-product', methods=['POST'])
 def saveProduct():
     code = request.form['code']
     name = request.form['name']
     price = int(request.form['price'] or 0)
-    db.product.insert({
-        'code': code,
-        'name': name,
-        'price': price
-    })
+    pid = request.form.get('pid')
+    product = {
+            'code': code, 
+            'name': name, 
+            'price': price
+    }
+
+    if not pid:
+        db.product.insert(product)
+    else:
+        db.product.update(
+            {'_id': ObjectId(pid)}, product)
+
     return redirect('/')
 
-from bson.objectid import ObjectId
 @app.route('/delete-product',methods=['POST'])
 def deleteProduct():
     pid = request.form['pid']
     db.product.remove({'_id': ObjectId(pid)})
     #return 'Pid:' + pid
     return redirect('/')
+
+@app.route('/add-product')
+def addProduct():
+    return render_template('product_form.html',p={})
+
+@app.route('/update-product/<pid>')
+def updateProduct(pid):
+    p = db.product.find_one({'_id':ObjectId(pid)})
+    return render_template('product_form.html',p=p)
 
 app.run(debug=True)
