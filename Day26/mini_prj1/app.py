4397915392c7a3043a3f55400bdaec018940c57c
Tung Duong <duongthanhtungvn01@gmail.com>
2021-06-08 20:32:09 +0700

    Update

--- Day26/mini_prj1/app.py
+++ Day26/mini_prj1/app.py
@@ -6,6 +6,7 @@ db_pass = 'abc@123'
 client = MongoClient(db_host, username=db_user, password=db_pass)
 db = client.db0001
 
+import time
 from flask import Flask, request, render_template, redirect
 app = Flask(__name__)
 
@@ -23,6 +24,14 @@ def createProduct():
         product = {f : request.form[f] for f in fields}
         product['price'] = int(product['price'] or 0)
         product['qty'] =  int(product['qty'] or 0)
+
+        file = request.files.get('image')
+        
+        if file is not None and file.filename != '':
+            filepath =  f'/static/images/{str(time.time())}.jpg'
+            file.save(app.root_path + filepath)
+            product['image'] = filepath
+
         db.product.insert(product)
         return redirect('/')
 
