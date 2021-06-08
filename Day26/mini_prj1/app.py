1d9a541153409098752be3d6db84b3223721c972
Tung Duong <duongthanhtungvn01@gmail.com>
2021-06-08 19:21:01 +0700

    Update

--- Day26/mini_prj1/app.py
+++ Day26/mini_prj1/app.py
@@ -1,7 +1,13 @@
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+db_host = '34.70.69.231'
+db_user = 'mongo'
+db_pass = 'abc@123'
+client = MongoClient(db_host, username=db_user, password=db_pass)
+db = client.db0001
+
 from flask import Flask, request, render_template, redirect
-
 app = Flask(__name__)
-
 @app.route('/')
 def index():
     return render_template('index.html')
