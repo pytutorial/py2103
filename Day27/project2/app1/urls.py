2ff481ee29988803e36a89e07ef890626aefce40
Tung Duong <duongthanhtungvn01@gmail.com>
2021-06-10 21:08:17 +0700

    Update

--- Day27/project2/app1/urls.py
+++ Day27/project2/app1/urls.py
@@ -1,7 +1,8 @@
 #app1/urls.py
 from django.urls import path 
-from .views import hello
+from .views import *
 
 urlpatterns = [
     path('hello', hello), #--> app1/hello
+    path('login', login),  #--> app1/login
 ]
--- Day27/project2/app1/views.py
+++ Day27/project2/app1/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import redirect, render
 
 # 127.0.0.1:8000/app1/hello
 def hello(request):
-    name = request.GET.get('name', '')
-    return render(request, 'hello.html', {'name': name}) #return HttpResponse("Hello " + name)
+    #name = request.GET.get('name', '')
+    name = request.session.get('username', '')
+    return render(request, 'hello.html', 
+                    {'name': name}) 
+
+# 127.0.0.1:8000/app1/login
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    else:  #POST
+        username = request.POST['username']
+        password = request.POST['password']
+        #TODO: Validate username/password
+        request.session['username'] = username
+        return redirect('/app1/hello')
