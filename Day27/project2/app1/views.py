060c5c96ef55e6bcce00f9d16c454d44569d6a99
Tung Duong <duongthanhtungvn01@gmail.com>
2021-06-10 20:31:12 +0700

    Update

--- Day27/project2/app1/views.py
+++ Day27/project2/app1/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render, HttpResponse
 # 127.0.0.1:8000/app1/hello
 def hello(request):
     name = request.GET.get('name', '')
-    return HttpResponse("Hello " + name)
+    return render(request, 'hello.html', {'name': name}) #return HttpResponse("Hello " + name)
