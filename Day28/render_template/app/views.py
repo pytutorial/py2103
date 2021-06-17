40cb99657a7f4e4455a645d8129eedacc009147b
Tung Duong <duongthanhtungvn01@gmail.com>
2021-06-17 20:22:12 +0700

    Update

--- Day28/render_template/app/views.py
+++ Day28/render_template/app/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 
+productList = [ 
+    {'id': 1, 'code': 'IPX', 
+        'name': 'IPhone', 'price': 10500000},
+        
+    {'id': 2, 'code': 'IP11', 
+        'name': 'IPhone 11', 'price': 11500000},
+
+    {'id': 3, 'code': 'IP12', 
+    'name': 'IPhone 12', 'price': 12500000}
+]
+
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'productList': productList
+    }
+    return render(request, 'index.html', context)
