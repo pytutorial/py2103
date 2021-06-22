e84f46438ed8090a414d7080ac0a42e0f79b1853
Tung Duong <duongthanhtungvn01@gmail.com>
2021-06-22 19:38:19 +0700

    Add static

--- Day29/upload_file/upload_file/settings.py
+++ Day29/upload_file/upload_file/settings.py
@@ -124,3 +124,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# New
+import os
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, "static"),
+]
