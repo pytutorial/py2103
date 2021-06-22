6affd6f0ec124825b52c7d9ee2f6c639a8c874e8
Tung Duong <duongthanhtungvn01@gmail.com>
2021-06-22 20:12:49 +0700

    Update

--- Day29/upload_file/app/views.py
+++ Day29/upload_file/app/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render, HttpResponse
+from django.core.files.storage import FileSystemStorage
 
+fs = FileSystemStorage()
 # Create your views here.
 def upload(request):
     file = request.FILES.get('image')
     if file != None and file.name != '':
-        print(file.name)
-        return HttpResponse('Uploaded')
+        file_path = 'static/' + file.name 
+        saved_path = fs.save(file_path, file)
+        print(saved_path)
+        return HttpResponse(f'<a href="/{saved_path}">{file.name}</a>')
     else:
         return HttpResponse('No file upload')
         
