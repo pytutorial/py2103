21570521ac69c445ad4064ed94badbe0c9bbb9ec
Tung Duong <duongthanhtungvn01@gmail.com>
2021-06-22 19:54:10 +0700

    Update

--- Day29/upload_file/app/urls.py
+++ Day29/upload_file/app/urls.py
@@ -2,5 +2,6 @@
 from django.urls import path
 from .views import *
 urlpatterns = [ 
+    path('upload', upload),
     path('', index),
 ]
--- Day29/upload_file/app/views.py
+++ Day29/upload_file/app/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def upload(request):
+    file = request.FILES.get('image')
+    if file != None and file.name != '':
+        print(file.name)
+        return HttpResponse('Uploaded')
+    else:
+        return HttpResponse('No file upload')
+        
 def index(request):
     return render(request, 'index.html')
