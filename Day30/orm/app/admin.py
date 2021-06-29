53d43116d0b41149f6168c9cb2a23ace08338b80
Tung Duong <duongthanhtungvn01@gmail.com>
2021-06-29 21:19:51 +0700

    Update

--- Day30/orm/app/admin.py
+++ Day30/orm/app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+admin.site.register(Customer)
+
+admin.site.register(Category)
+admin.site.register(Product)
--- Day30/orm/app/models.py
+++ Day30/orm/app/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+    address = models.CharField(max_length=500)
+    gender = models.CharField(max_length=20, blank=True) # MALE/FEMALE
+    birthDate = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    code = models.CharField(max_length=30, unique=True)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.PROTECT) #SET_NULL(null=True)/CASCADE
+    code = models.CharField(max_length=20, unique=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+# python manage.py makemigrations --> SQL generator
+# python manage.py migrate        --> SQL executor
+# python manage.py shell
+
