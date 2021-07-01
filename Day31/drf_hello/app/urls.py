60a6d92bec7e265ca1396f2b8a5a8492f6b4b905
Tung Duong <duongthanhtungvn01@gmail.com>
2021-07-01 19:59:08 +0700

    Update

--- Day31/drf_hello/app/urls.py
+++ Day31/drf_hello/app/urls.py
@@ -2,5 +2,6 @@
 from django.urls import path
 from .views import *
 urlpatterns = [ 
+    path('login', login),
     path('hello', hello),
 ]
--- Day31/drf_hello/app/views.py
+++ Day31/drf_hello/app/views.py
@@ -2,7 +2,21 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-#http://127.0.0.1:8000/api/hello
+#http://127.0.0.1:8000/api/hello?name=Nguyen+Van+A
 @api_view(['GET'])
 def hello(request):
-    return Response({'message': 'Hello'})
+    name = request.GET.get('name', '')
+    return Response({'message': f'Hello {name}'})
+
+@api_view(['POST'])
+def login(request):  #  http:/127.0.0.1:8000/api/login
+    username = request.data.GET('username', '')
+    password = request.data.GET('password', '')
+
+    if username != 'admin':
+        return Response({'error': 'Tên đăng nhập không tồn tại'})
+    
+    if password != '1234':
+        return Response({'error': 'Mật khẩu không chính xác'})
+
+    return Response({'success': True})
