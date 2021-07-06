331b27129743eb01ce8094de55aa40d7e6c29a2a
Tung Duong <duongthanhtungvn01@gmail.com>
2021-07-06 20:48:34 +0700

    Update

--- Day31/drf_hello/drf_hello/settings.py
+++ Day31/drf_hello/drf_hello/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
-
+CORS_ORIGIN_ALLOW_ALL = True
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
