c4d13231f128be6815d382568e1af938ea3ec971
Tung Duong <duongthanhtungvn01@gmail.com>
2021-07-20 21:13:40 +0700

    Update

--- Day35/backend/app/urls.py
+++ Day35/backend/app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('create-category', createCategory),
     path('hello', hello)
 ]
--- Day35/backend/app/views.py
+++ Day35/backend/app/views.py
@@ -1,6 +1,22 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
+from rest_framework.serializers import ModelSerializer
+from .models import *
+class CategorySerializer(ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+@api_view(['POST'])
+def createCategory(request):
+    serializer = CategorySerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response({'success': True})
+    else:
+        return Response({'errors': serializer.errors})
+
 @api_view(['GET'])
 def hello(request):
     return Response({'message': 'Hello'})
