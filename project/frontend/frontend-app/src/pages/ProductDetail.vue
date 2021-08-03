76bc2d108bcb1bd87d9286fc366d6cd2f47b8f81
Tung Duong <duongthanhtungvn01@gmail.com>
2021-08-03 21:18:16 +0700

    Update

--- project/frontend/frontend-app/src/pages/ProductDetail.vue
+++ project/frontend/frontend-app/src/pages/ProductDetail.vue
@@ -1,3 +1,41 @@
 <template>
-    <div>Product Detail</div>
+  <div class="container mt-5 mb-5">
+    <div class="row">
+      <div class="col-6">
+        <img
+          class="product-detail-image"
+          src="https://raw.githubusercontent.com/pytutorial/themes/master/shop_themes/images/sample.jpg"
+        />
+      </div>
+
+      <div class="col-6 mt-5">
+        <div class="product-detail-title">Acer 001</div>
+        <br />
+        <table class="table">
+          <tr>
+            <td>Hãng sản xuất:</td>
+            <td>Acer</td>
+          </tr>
+          <tr>
+            <td>Giá bán:</td>
+            <td><b>6.500.000 ₫</b></td>
+          </tr>
+        </table>
+        <br />
+        <a class="btn btn-secondary" href="index.html">Quay lại</a>
+        <a class="btn btn-primary" href="order_product.html">Mua hàng</a>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+    .product-detail-title {
+        font-size: 24px;
+        font-weight: bold;
+    }
+
+    .product-detail-image {
+        width: 100%
+    }
+</style>
