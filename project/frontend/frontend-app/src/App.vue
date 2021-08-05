f4f9ce02ba793d920fdd98c6abb96f44be7c9c3e
Tung Duong <duongthanhtungvn01@gmail.com>
2021-08-05 20:14:34 +0700

    Update

--- project/frontend/frontend-app/src/App.vue
+++ project/frontend/frontend-app/src/App.vue
@@ -1,5 +1,31 @@
 <template>
-    <div>
-        <router-view></router-view>
+  <div>
+    <div class="bg-primary">
+      <div class="container">
+        <nav class="navbar navbar-expand navbar-dark bg-primary p-0">
+          <ul class="navbar-nav">
+            <li class="nav-item" :class="{ active: current_page == 1 }">
+              <router-link class="nav-link" to="/staff">Quản lý nhóm sản phẩm</router-link>
+            </li>
+            <li class="nav-item" :class="{ active: current_page == 2 }">
+              <router-link class="nav-link" to="/staff/product">Quản lý sản phẩm</router-link>
+            </li>
+            <li class="nav-item" :class="{ active: current_page == 3 }">
+              <router-link class="nav-link" to="/staff/order">Quản lý đơn hàng</router-link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
+    <router-view></router-view>
+  </div>
 </template>
+<script>
+export default {
+  computed:{
+    current_page: function() {
+        return this.$route.meta.page;
+    }
+  }
+}
+</script>
