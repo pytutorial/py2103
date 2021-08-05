6219152772bc0045582a1e381a4cdec83613bbe5
Tung Duong <duongthanhtungvn01@gmail.com>
2021-08-05 19:43:44 +0700

    Update

--- project/frontend/frontend-app/src/pages/staff/category/CategoryList.vue
+++ project/frontend/frontend-app/src/pages/staff/category/CategoryList.vue
@@ -1,3 +1,45 @@
 <template>
-    <div>Category List</div>
+    <div id='app' class="container mt-3">
+        <div class="row">
+            <div class="col">
+                <a href='category_form.html' class="btn btn-primary float-right">Thêm sản phẩm</a>
+            </div>
+        </div>
+
+        <table class="table table-bordered mt-3">
+            <thead>
+                <tr>
+                    <th>STT</th>
+                    <th>Mã</th>
+                    <th>Tên</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
+<script>
+export default{
+    data: () => ({
+        keyword: "",
+        page: 1,
+        total: 0,
+        items: [],
+        pageSize: 5,
+        baseUrl: 'http://127.0.0.1:8000'
+    }),
+    methods: {
+        fetch_data: async function(){
+        }
+    },
+    mounted: async function(){
+        this.fetch_data()
+    }
+}
+</script>
