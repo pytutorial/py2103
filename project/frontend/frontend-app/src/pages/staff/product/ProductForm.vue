d4b3b76a904e48ed838ef0f120f3f09b943b0401
Tung Duong <duongthanhtungvn01@gmail.com>
2021-08-05 20:04:03 +0700

    Update

--- project/frontend/frontend-app/src/pages/staff/product/ProductForm.vue
+++ project/frontend/frontend-app/src/pages/staff/product/ProductForm.vue
@@ -1,3 +1,48 @@
 <template>
-    <div>Product Form</div>
+    <div id="app" class="container mt-3">
+        <form id="fmt" @submit.prevent="save()" class="col-9">
+            <table class="table">
+                <tbody>
+                    <tr>
+                        <td><label>Mã:</label></td>
+                        <td><input class="form-control" name="code"></td>
+                    </tr>
+                    <tr>
+                        <td><label>Tên:</label></td>
+                        <td><input class="form-control" name="name"></td>
+                    </tr>
+                    <tr>
+                        <td><label>Mô tả:</label></td>
+                        <td><textarea class="form-control" name="description"></textarea></td>
+                    </tr>
+                    <tr>
+                        <td><label>Đơn giá:</label></td>
+                        <td><input class="form-control" name="price"></td>
+                    </tr>
+                    <tr>
+                        <td><label>Ảnh:</label></td>
+                        <td><input type='file' class="form-control-file" name="image"></td>
+                    </tr>
+                </tbody>
+            </table>
+            
+            <button class="btn btn-primary">Lưu lại</button>
+        </form>
+    </div>
 </template>
+<script>
+export default {
+    data: () => ({
+        name: "",
+    }),
+    methods: {
+        save: async function(){
+            let data = new FormData(document.getElementById('fmt'));
+            console.log(data.get('name'), data.get('code'));
+        }
+    },
+    mounted: function(){
+        
+    }
+}
+</script>
