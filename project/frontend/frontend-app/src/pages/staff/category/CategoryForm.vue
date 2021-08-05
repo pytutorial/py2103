fbb4d364b09b80901084dcef8a4a8fa0e05c280b
Tung Duong <duongthanhtungvn01@gmail.com>
2021-08-05 19:53:23 +0700

    Update

--- project/frontend/frontend-app/src/pages/staff/category/CategoryForm.vue
+++ project/frontend/frontend-app/src/pages/staff/category/CategoryForm.vue
@@ -1,3 +1,36 @@
 <template>
-    <div>Category Form</div>
+    <div id="app" class="container mt-3">
+        <form id="fmt" @submit.prevent="save()" class="col-9">
+            <table class="table">
+                <tbody>
+                    <tr>
+                        <td><label>Mã:</label></td>
+                        <td><input class="form-control" name="code"></td>
+                    </tr>
+                    <tr>
+                        <td><label>Tên:</label></td>
+                        <td><input class="form-control" name="name"></td>
+                    </tr>
+                </tbody>
+            </table>
+            
+            <button class="btn btn-primary">Lưu lại</button>
+        </form>
+    </div>
 </template>
+<script>
+export default{
+    data: () => ({
+        name: "",
+    }),
+    methods: {
+        save: async function(){
+            let data = new FormData(document.getElementById('fmt'));
+            console.log(data.get('name'), data.get('code'));
+        }
+    },
+    mounted: function(){
+        
+    }
+}
+</script>
